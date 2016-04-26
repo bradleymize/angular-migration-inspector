c84511db455dabbe442b2d3815197c7ff043419b
Bradley Mize <bmize@co.pierce.wa.us>
2016-04-26 13:19:34 -0700

    Initial plugin commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var esprima = require("esprima");
+var globby = require("globby");
+var htmlparser2 = require("htmlparser2");
+var lex = require("lex");
+var semver = require("semver");
+
+//Constructor
+var AngularMigrationInspector = function(version) {
+
+};
+
+module.exports = AngularMigrationInspector;
